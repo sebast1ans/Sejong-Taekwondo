1ec87c6809c5330f8a8a9e45b5d8c7042a9a79fc
sebast1ans <sebastiantran@me.com>
2020-10-31 03:32:44 +0100

    navbar improvements

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,7 @@
 <template>
-    <scrollactive id="nav" :duration="800" bezier-easing-value=".66,0,.33,1" :modify-url=false>
-        <v-container class="d-flex justify-space-between align-center py-1">
+    <scrollactive id="nav" :duration="800" bezier-easing-value=".66,0,.33,1" :modify-url=false
+                  :class="{navShrink: scrolledBeyond}">
+        <v-container class="d-flex justify-space-between align-center py-1" @scroll="log">
             <img src="../assets/images/logos/whiteLogo.svg" alt="" class="nav-logo" height="85"
                  @click="$vuetify.goTo('#welcome', options)">
 
@@ -44,11 +45,20 @@ export default {
                 {target: '#what-is-taekwondo', text: 'O Taekwondo'},
                 {target: '#locations', text: 'Tréninky'},
                 {target: '#contact', text: 'Kontakt'},
-            ]
+            ],
+            scrolledBeyond: false
         }
     },
+    created() {
+        window.addEventListener('scroll', this.log);
+    },
+    destroyed() {
+        window.removeEventListener('scroll', this.log);
+    },
     methods: {
-
+        log() {
+            return window.scrollY > 120 ? this.scrolledBeyond = true : this.scrolledBeyond = false
+        }
     },
     computed: {
         options() {
@@ -56,7 +66,8 @@ export default {
                 duration: 700,
                 easing: 'easeInOutQuart'
             }
-        }
+        },
+
     }
 
 }
