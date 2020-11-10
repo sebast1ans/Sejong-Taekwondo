a54f4f78c74f7f8d1336b5a3c3ff927394498f56
sebast1ans <sebastiantran@me.com>
2020-11-10 16:43:02 +0100

    another tweak

--- src/components/WhatIsTaekwondo.vue
+++ src/components/WhatIsTaekwondo.vue
@@ -47,22 +47,18 @@ export default {
                     name: "Taekwondo",
                     imgSrc: 'foto1.jpg',
                     content: `
-                    <br><img src=${require("@/assets/images/what-is-taekwondo/content/kihap.jpg")}
-                                             style="display: inline-block; width: 45%; float: right; margin-left: 1em; margin-bottom: 1em" alt="kihap">
+                    <br><img src=${require("@/assets/images/what-is-taekwondo/content/kihap.jpg")} style="display: inline-block; width: 45%; float: right; margin-left: 1em; margin-bottom: 1em" alt="kihap">
                                         <p>Taekwondo je <strong>bojové umění</strong>, které se nezávisle vyvíjelo
                                             <strong>v Koreji</strong> po dobu více než dvě tisíciletí s stalo se tak
                                             <strong>moderním mezinárodním sportem</strong>. Stručně lze Taekwondo
                                             charakterizovat jako bojové umění, které používá ruce, nohy a ostatní části
                                             těla k odvrácení útoku protivníka.</p>
-
                                         <p>Jelikož Taekwondo se již od počátku vyvíjelo jako sebeobranný systém, tak i
                                             veškeré aktivity v Taekwondo se zakládají v duchu sebeobrany. V dávných
                                             dobách lidé žily jednoduchý život s nedostatkem fyzického zatížení a ve
                                             stáří se jejich těla hrbila. Taekwondo také napomáhá upevnit zdraví,
                                             fyzickou i psychickou sílu a celkovou rovnováhu lidského těla.</p>
-                                        <img src=${require("@/assets/images/what-is-taekwondo/content/concentrating.jpg")}
-                                             style="display:inline-block; width: 40%; margin-right: 1em; float: left;"
-                                             alt="concentrating">
+                                        <img src=${require("@/assets/images/what-is-taekwondo/content/concentrating.jpg")} style="display:inline-block; width: 40%; margin-right: 1em; float: left;" alt="concentrating">
                                         <p>Člověk, který se věnuje Taekwondo se musí naučit potlačit vlastní ego,
                                             ovládat své nitro a být uctivý k druhým, čili dospět k harmonii duchovna a
                                             fyzické síly, protože se učí technikám pro vlastní obranu s použitím celého
@@ -97,8 +93,8 @@ export default {
                     imgSrc: 'foto2.jpg',
                     content: `<br>
                                   <video id="poomsae-vid" width="100%" controls src="${require('@/assets/videos/poomsae.mp4')}"></video>
-                                        <br>
-                                        <p>Poomsae (souborná cvičení) jsou závazná cvičení, vypracovaná mistry
+                                  <br>
+                                  <p>Poomsae (souborná cvičení) jsou závazná cvičení, vypracovaná mistry
                                             Taekwondo,
                                             za účelem předávání vědy o umění boje a vlastních interpretacích tohoto
                                             umění.</p>
@@ -174,27 +170,21 @@ export default {
                     imgSrc: 'foto4.jpg',
                     content: `
                     <br>
-                    <img src=${require("@/assets/images/what-is-taekwondo/content/kyokpa1.jpg")}
-                                             style="display: inline-block; width: 35%; float: right; margin-left: 1em"
-                                              alt="kyokpa">
+                    <img src=${require("@/assets/images/what-is-taekwondo/content/kyokpa1.jpg")} style="display: inline-block; width: 35%; float: right; margin-left: 1em" alt="kyokpa">
                                         <p>Kyokpa (přerážecí techniky) se dělí na <strong>silové přerážení</strong>,
                                             kde se zaměřuje především na sílu prováděné techniky, a <strong>speciální
                                                 přerážení</strong>, kde je nejdůležitějším faktorem obtížnost prováděné
                                             techniky. Kjokpcha jsou součástí výcviku a jsou i soutěže v této disciplíně.
                                             Cvičenec jimi testuje sama sebe, svoji šikovnost, sílu, přesnost, zručnost,
                                             schopnost soustředit se a tím i svoji vyspělost.</p>
-                                        <img src=${require("@/assets/images/what-is-taekwondo/content/kyokpa2.jpg")}
-                                              style="width: 100%; margin: auto" alt="kyokpa">
-                                        <img src=${require("@/assets/images/what-is-taekwondo/content/kyokpa3.jpg")}
-                                              style="width: 100%; margin: auto" alt="kyokpa">
+                                        <img src=${require("@/assets/images/what-is-taekwondo/content/kyokpa2.jpg")} style="width: 100%; margin: auto" alt="kyokpa">
+                                        <img src=${require("@/assets/images/what-is-taekwondo/content/kyokpa3.jpg")} style="width: 100%; margin: auto" alt="kyokpa">
                     `,
                     dialog: false
                 }
             ]
         }
     },
-
-    methods: {}
 }
 </script>
 
