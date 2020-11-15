d74898b816412e4431e9ce902a7fb9b5092c65b4
sebast1ans <sebastiantran@me.com>
2020-11-15 02:44:45 +0100

    login mod

--- src/main.js
+++ src/main.js
@@ -5,6 +5,8 @@ import vuetify from './plugins/vuetify'
 import VueScrollactive from 'vue-scrollactive';
 import 'material-design-icons/iconfont/material-icons.css'
 import Vuetify from "vuetify";
+import firebase from 'firebase/app'
+import 'firebase/auth'
 
 Vue.use(VueScrollactive);
 Vue.config.productionTip = false;
@@ -14,8 +16,15 @@ Vue.filter('snippet', function (value) {
     return value.length > 190 ? `${value.slice(0, 190)}...` : value
 })
 
-new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app');
+let app
+
+firebase.auth().onAuthStateChanged(user => {
+    if (!app) {
+        app = new Vue({
+            router,
+            vuetify,
+            render: h => h(App)
+        }).$mount('#app');
+    }
+})
+
